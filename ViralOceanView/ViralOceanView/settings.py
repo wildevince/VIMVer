4de799085c83239b8978000796ef245b5b91cb7d
wildevince <wildevince@gmail.com>
2022-01-26 17:30:52 +0100

    static collecting  3rd attempt

--- ViralOceanView/ViralOceanView/settings.py
+++ ViralOceanView/ViralOceanView/settings.py
@@ -132,9 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    path.join(BASE_DIR, 'ViralOceanView', 'static'),
-)
+#STATICFILES_DIRS = (path.join(BASE_DIR, 'ViralOceanView', 'static'),)
 STATIC_ROOT = path.join(Temp_DIR, 'static')
 
 
