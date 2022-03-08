afb060b00a80452806520f459899641b1d888544
wildevince <wildevince@gmail.com>
2022-03-08 10:30:45 +0100

    oups

--- run_VIMVer.sh
+++ run_VIMVer.sh
@@ -1,5 +1,5 @@
 
-fi ($1 -eq '--clear'); do for file in `l ~/VIMVer/ViralOceanView/ViralOceanView/file/OceanFinder/out`; do rm $file; done; fi 
+if ($1 -eq '--clear'); do for file in `l ~/VIMVer/ViralOceanView/ViralOceanView/file/OceanFinder/out`; do rm $file; done; fi 
 
 
 firefox http://10.1.2.200:8000/ &
