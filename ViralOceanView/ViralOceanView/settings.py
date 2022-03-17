b7b84f622e918e02a4cfc49a4a9e73155412c506
wildevince <wildevince@gmail.com>
2022-03-17 11:43:26 +0100

    erratum 2

--- ViralOceanView/ViralOceanView/settings.py
+++ ViralOceanView/ViralOceanView/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # 'localhost', '127.0.0.1'
 # 'VM', '10.1.2.200'
 # 'VM', '0.0.0.0:8000'
-ALLOWED_HOSTS = ['127.0.0.1', '10.1.2.200', '139.124.83.212', 'https://vimver.afmb.univ-mrs.fr:8000/'] 
+ALLOWED_HOSTS = ['127.0.0.1', '10.1.2.200', '139.124.83.212', 'vimver.afmb.univ-mrs.fr'] 
 
 # Application definition
 
